9e0b8f37c0995daf179c0df0b9da8ecd7829fd1a
kaushick-n <kaushick.natarajan@gmail.com>
2026-03-02 14:56:45 +0530

    validate update

--- server/app/routes/validate.py
+++ server/app/routes/validate.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.schemas.idea_schema import IdeaRequest
+from app.schemas.idea_schema import IdeaResponse
 from app.controllers.validate_controller import validate_idea
 
 router = APIRouter()
